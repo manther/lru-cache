72f99a5252e9b7d86ae1a724bf1e4417205e5ea7
Manther <jason.manther.young@gmail.com>
2016-05-25 14:10:45 -0500

    LRU Cache.

---
+++ lib/lru/cache/node.rb
@@ -0,0 +1,10 @@
+class Node
+  attr_accessor :value, :next_node, :prev_node, :key
+
+  def initialize(value, key, prev_node, next_node)
+    @value     = value
+    @prev_node = prev_node
+    @next_node = next_node
+    @key       = key
+  end
+end
